fa439cb00dc8e02268f4d7e8443e5d8e01d3b50b
nathan m <124533993+nathanmgithub@users.noreply.github.com>
2024-10-29 23:08:11 -0300

    Create ItemDoPedido.java

---
+++ projeto/scr/restaurante/ItemDoPedido.java
@@ -0,0 +1,29 @@
+package restaurante;
+
+public class ItemDoPedido {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public ItemDoPedido(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double calcularSubtotal() {
+        return preco * quantidade;
+    }
+}
